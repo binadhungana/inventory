36d01ccdc14f088cbeafe4cf7f58de3e73c5aec6
binadhungana <beenadhungana990@gmail.com>
2025-05-06 17:09:06 +0545

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import Tailwindcss from "@tailwindcss/vite"; 
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), Tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, './src'),
     },
   },
 });
