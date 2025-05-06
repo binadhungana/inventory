3639c63501b44d6f06526c39d592e3f3de87d905
binadhungana <beenadhungana990@gmail.com>
2025-05-06 16:18:34 +0545

    fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
+import Home from "./Pages/Home";
 
-import { CartProvider } from "./pages/CartContext";
+import { CartProvider } from "./Pages/CartContext";
 import { ToastContainer } from "react-toastify";
 import Cart from "./components/Cart";
-import Login from "./pages/Login";
-import SignUp from "./pages/Signup";
+import Login from "./Pages/Login";
+import SignUp from "./Pages/Signup";
 
 function App() {
   return (
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -8,7 +8,7 @@ import {
   DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
-import { CartContext } from "../pages/CartContext";
+import { CartContext } from "../Pages/CartContext";
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
